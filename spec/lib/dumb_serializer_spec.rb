e5b7fbb0bf34a3e822da355a0efee9e0cfb8e9e7
hedselu <robspawlas@gmail.com>
2017-02-26 01:43:39 +0100

    Applied specs for dump_serializer.

--- spec/lib/dumb_serializer_spec.rb
+++ spec/lib/dumb_serializer_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe DumbSerializer do
+  let(:object) { Car.new }
 
+  it { expect(object).to respond_to(:dump) }
+  it { expect(Car).to respond_to(:find) }
+  it { expect(Car).to respond_to(:all) }
+  it { expect(Car).to respond_to(:remove) }
+  it { expect(Car).to respond_to(:drop) }
+  it { expect(Car).to respond_to(:db_config) }
+  it { expect(Car).to respond_to(:db_config=) }
 end
